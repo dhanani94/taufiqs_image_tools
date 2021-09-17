de9340d176d394f10410e5e6aa381844b8320825
taufiq <dhanani94@gmail.com>
2021-09-17 06:55:28 -0400

    adds the duplicates thing

---
+++ examples/find_duplicate.py
@@ -0,0 +1,81 @@
+### this is what's going to go donw
+#  going to take in a target directory
+# go through it and save all of hte files/checksum perhaps even read the exif
+# then 
+
+import hashlib
+import os
+import sys
+
+
+# source: https://www.pythoncentral.io/finding-duplicate-files-with-python/
+
+
+def find_dup(parentFolder):
+    # Dups in format {hash:[names]}
+    dups = {}
+    for dirName, subdirs, fileList in os.walk(parentFolder):
+        print('Scanning %s...' % dirName)
+        for filename in fileList:
+            # Get the path to the file
+            path = os.path.join(dirName, filename)
+            # Calculate hash
+            file_hash = hash_file(path)
+            # Add or append the file path
+            if file_hash in dups:
+                dups[file_hash].append(path)
+            else:
+                dups[file_hash] = [path]
+    return dups
+
+
+# Joins two dictionaries
+def join_dicts(dict1, dict2):
+    for key in dict2.keys():
+        if key in dict1:
+            dict1[key] = dict1[key] + dict2[key]
+        else:
+            dict1[key] = dict2[key]
+
+
+def hash_file(path, blocksize=65536):
+    afile = open(path, 'rb')
+    hasher = hashlib.md5()
+    buf = afile.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    afile.close()
+    return hasher.hexdigest()
+
+
+def print_results(dict1):
+    results = list(filter(lambda x: len(x) > 1, dict1.values()))
+    if len(results) > 0:
+        print('Duplicates Found:')
+        print('The following files are identical. The name could differ, but the content is identical')
+        print('___________________')
+        for result in results:
+            for subresult in result:
+                print('\t\t%s' % subresult)
+            print('___________________')
+
+    else:
+        print('No duplicate files found.')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        dups = {}
+        folders = sys.argv[1:]
+        for i in folders:
+            # Iterate the folders given
+            if os.path.exists(i):
+                # Find the duplicated files and append them to the dups
+                join_dicts(dups, find_dup(i))
+            else:
+                print('%s is not a valid path, please verify' % i)
+                sys.exit()
+        print_results(dups)
+    else:
+        print('Usage: python dupFinder.py folder or python dupFinder.py folder1 folder2 folder3')
